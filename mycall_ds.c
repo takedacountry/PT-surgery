5cfba7c2ee2374eb75bb3369f00a102b71a8e1a6
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-01-18 19:15:05 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -34,53 +34,53 @@ int main(void)
     char *mb = (char*)malloc(INDEX);
     char *mc = (char*)malloc(INDEX);
     char *md = (char*)malloc(INDEX);
-    char *me = (char*)malloc(INDEX);
-    char *mf = (char*)malloc(INDEX);
-    char *mg = (char*)malloc(INDEX);
-    char *mh = (char*)malloc(INDEX);
-    char *mi = (char*)malloc(INDEX);
-    char *mj = (char*)malloc(INDEX);
-    char *mk = (char*)malloc(INDEX);
-    char *ml = (char*)malloc(INDEX);
-    char *mm = (char*)malloc(INDEX);
-    char *mn = (char*)malloc(INDEX);
-    char *mo = (char*)malloc(INDEX);
-    char *mp = (char*)malloc(INDEX);
+    // char *me = (char*)malloc(INDEX);
+    // char *mf = (char*)malloc(INDEX);
+    // char *mg = (char*)malloc(INDEX);
+    // char *mh = (char*)malloc(INDEX);
+    // char *mi = (char*)malloc(INDEX);
+    // char *mj = (char*)malloc(INDEX);
+    // char *mk = (char*)malloc(INDEX);
+    // char *ml = (char*)malloc(INDEX);
+    // char *mm = (char*)malloc(INDEX);
+    // char *mn = (char*)malloc(INDEX);
+    // char *mo = (char*)malloc(INDEX);
+    // char *mp = (char*)malloc(INDEX);
     
     memset(ma, 0, INDEX);
     memset(mb, 0, INDEX);
     memset(mc, 0, INDEX);
     memset(md, 0, INDEX);
-    memset(me, 0, INDEX);
-    memset(mf, 0, INDEX);
-    memset(mg, 0, INDEX);
-    memset(mh, 0, INDEX);
-    memset(mi, 0, INDEX);
-    memset(mj, 0, INDEX);
-    memset(mk, 0, INDEX);
-    memset(ml, 0, INDEX);
-    memset(mm, 0, INDEX);
-    memset(mn, 0, INDEX);
-    memset(mo, 0, INDEX);
-    memset(mp, 0, INDEX);
+    // memset(me, 0, INDEX);
+    // memset(mf, 0, INDEX);
+    // memset(mg, 0, INDEX);
+    // memset(mh, 0, INDEX);
+    // memset(mi, 0, INDEX);
+    // memset(mj, 0, INDEX);
+    // memset(mk, 0, INDEX);
+    // memset(ml, 0, INDEX);
+    // memset(mm, 0, INDEX);
+    // memset(mn, 0, INDEX);
+    // memset(mo, 0, INDEX);
+    // memset(mp, 0, INDEX);
     
 
     printf("va: %p\n", ma); // print user va 
     printf("va: %p\n", mb); // print user va 
     printf("va: %p\n", mc); // print user va 
     printf("va: %p\n", md); // print user va 
-    printf("va: %p\n", me); // print user va
-    printf("va: %p\n", mf); // print user va
-    printf("va: %p\n", mg); // print user va
-    printf("va: %p\n", mh); // print user va
-    printf("va: %p\n", mi); // print user va
-    printf("va: %p\n", mj); // print user va
-    printf("va: %p\n", mk); // print user va
-    printf("va: %p\n", ml); // print user va
-    printf("va: %p\n", mm); // print user va
-    printf("va: %p\n", mn); // print user va
-    printf("va: %p\n", mo); // print user va
-    printf("va: %p\n", mp); // print user va
+    // printf("va: %p\n", me); // print user va
+    // printf("va: %p\n", mf); // print user va
+    // printf("va: %p\n", mg); // print user va
+    // printf("va: %p\n", mh); // print user va
+    // printf("va: %p\n", mi); // print user va
+    // printf("va: %p\n", mj); // print user va
+    // printf("va: %p\n", mk); // print user va
+    // printf("va: %p\n", ml); // print user va
+    // printf("va: %p\n", mm); // print user va
+    // printf("va: %p\n", mn); // print user va
+    // printf("va: %p\n", mo); // print user va
+    // printf("va: %p\n", mp); // print user va
 
     // printf("%ld\n", syscall(SYS_mycall_ds_make));
     printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
@@ -109,18 +109,18 @@ int main(void)
     memset(mb, 1, INDEX);
     memset(mc, 1, INDEX);
     memset(md, 1, INDEX);
-    memset(me, 1, INDEX);
-    memset(mf, 1, INDEX);
-    memset(mg, 1, INDEX);
-    memset(mh, 1, INDEX);
-    memset(mi, 1, INDEX);
-    memset(mj, 1, INDEX);
-    memset(mk, 1, INDEX);
-    memset(ml, 1, INDEX);
-    memset(mm, 1, INDEX);
-    memset(mn, 1, INDEX);
-    memset(mo, 1, INDEX);
-    memset(mp, 1, INDEX);
+    // memset(me, 1, INDEX);
+    // memset(mf, 1, INDEX);
+    // memset(mg, 1, INDEX);
+    // memset(mh, 1, INDEX);
+    // memset(mi, 1, INDEX);
+    // memset(mj, 1, INDEX);
+    // memset(mk, 1, INDEX);
+    // memset(ml, 1, INDEX);
+    // memset(mm, 1, INDEX);
+    // memset(mn, 1, INDEX);
+    // memset(mo, 1, INDEX);
+    // memset(mp, 1, INDEX);
 
     // for(char *t = ma; t < ma + 100; t++){
     //     printf("%d ", *t);
@@ -130,18 +130,18 @@ int main(void)
     free(mb);
     free(mc);
     free(md);
-    free(me);
-    free(mf);
-    free(mg);
-    free(mh);
-    free(mi);
-    free(mj);
-    free(mk);
-    free(ml);
-    free(mm);
-    free(mn);
-    free(mo);
-    free(mp);
+    // free(me);
+    // free(mf);
+    // free(mg);
+    // free(mh);
+    // free(mi);
+    // free(mj);
+    // free(mk);
+    // free(ml);
+    // free(mm);
+    // free(mn);
+    // free(mo);
+    // free(mp);
     
 
     printf("%ld\n", syscall(SYS_mycall_ds_delete));
