21a875949c6c313450c7f0ec047b64870b9bf3f2
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-02-27 13:16:08 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -93,7 +93,7 @@ int main(void)
     printf("%ld\n", syscall(SYS_mycall_m_search));
     
     // printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
-    printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0));
+    printf("%ld\n", syscall(SYS_mycall_recover_pgtable, ffff91970ae29000));
     
     printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
     printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
