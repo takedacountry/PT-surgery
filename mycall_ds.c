350c9143a9e80ec552bcef4c78b644ecdc1d75bd
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-03-08 14:32:51 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -86,7 +86,7 @@ int main(void)
     printf("va: %p\n", mp); // print user va
 
     printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
-    // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable));
+    printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable));
     printf("%ld\n", syscall(SYS_mycall_ds_make_user));
     // printf("%ld\n", syscall(SYS_mycall_ds_make_kernel));
     printf("%ld\n", syscall(SYS_mycall_ds_search));
