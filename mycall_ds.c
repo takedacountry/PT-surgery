871dc2bb5ec977bbab91420f6d25b33d980cb15f
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-03-11 14:58:15 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -88,15 +88,15 @@ int main(void)
     printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
     printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable));
     printf("%ld\n", syscall(SYS_mycall_ds_make_user));
-    // printf("%ld\n", syscall(SYS_mycall_ds_make_kernel));
+    printf("%ld\n", syscall(SYS_mycall_ds_make_kernel));
     printf("%ld\n", syscall(SYS_mycall_ds_search));
     printf("%ld\n", syscall(SYS_mycall_m_search));
     
     // printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
     printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
     
-    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
-    printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
+    // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
+    // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
 
     // success
     // for(char *t = ma; t < ma + INDEX; t++){
