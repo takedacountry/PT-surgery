a1dd4b2a3bbba4f4c0b4ad63a679b780360e883d
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-03-15 15:11:10 +0900

    Update ds.h

--- arch/x86/include/asm/ds.h
+++ arch/x86/include/asm/ds.h
@@ -32,11 +32,34 @@ struct m_list_head{
 	struct list_head ker_m_list;
 };
 
-// static LIST_HEAD(ds_list_head);
-// static LIST_HEAD(m_list_head);
+struct m_list_head *m_list;
+struct ds_list_head *ds_list;
 
-extern struct m_list_head *m_list;
-extern struct ds_list_head *ds_list;
+void init_ds_list_head(void)
+{
+        ds_list = kmalloc(sizeof(struct ds_list_head), GFP_KERNEL);
+        if(!ds_list)
+                return;
+        INIT_LIST_HEAD(&ds_list->usr_ds_list);
+        INIT_LIST_HEAD(&ds_list->ker_ds_list);
+        printk(KERN_INFO "init ds list head\n");
+}
 
-extern int make_usr_list(unsigned long address, pte_t *ptep);
-extern int make_ker_list(unsigned long address, pte_t *ptep);
+void init_m_list_head(void)
+{
+        m_list = kmalloc(sizeof(struct m_list_head), GFP_KERNEL);
+        if(!m_list)
+                return;
+        INIT_LIST_HEAD(&m_list->usr_m_list);
+        INIT_LIST_HEAD(&m_list->ker_m_list);
+        printk(KERN_INFO "init m list head\n");
+}
+
+void free_list_head(void)
+{
+        kfree(ds_list);
+        kfree(m_list);
+}
+
+int make_usr_list(unsigned long address, pte_t *ptep);
+int make_ker_list(unsigned long address, pte_t *ptep);
