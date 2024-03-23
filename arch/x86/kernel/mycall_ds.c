2eea264ac9ffffb5e8600cd767f0046061a9dc44
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-03-23 12:31:49 +0900

    Update mycall_ds.c

--- arch/x86/kernel/mycall_ds.c
+++ arch/x86/kernel/mycall_ds.c
@@ -153,6 +153,8 @@ static int search_pgtable_get_pfn(unsigned long pgd, unsigned long pud, unsigned
 
 struct m_list_head *m_list;
 struct ds_list_head *ds_list;
+EXPORT_SYMBOL(ds_list);
+EXPORT_SYMBOL(m_list);
 
 void init_ds_list_head(void)
 {
