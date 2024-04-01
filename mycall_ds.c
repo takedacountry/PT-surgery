51b43feeff5919c4c363bd4f35675c46501bcb02
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-04-01 14:13:49 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -87,16 +87,16 @@ int main(void)
     printf("va: %p\n", mo); // print user va
     printf("va: %p\n", mp); // print user va
 
-    printf("%ld\n", syscall(SYS_mycall_ds_init));
+    // printf("%ld\n", syscall(SYS_mycall_ds_init));
     // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable));
-    printf("%ld\n", syscall(SYS_mycall_ds_make_user));
-    printf("%ld\n", syscall(SYS_mycall_ds_make_kernel));
+    // printf("%ld\n", syscall(SYS_mycall_ds_make_user));
+    // printf("%ld\n", syscall(SYS_mycall_ds_make_kernel));
     printf("%ld\n", syscall(SYS_mycall_ds_search));
     printf("%ld\n", syscall(SYS_mycall_m_search));
     
     // printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
-    printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
+    // printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
     
     // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
