566fadd7192ecade6ec42504891c4fd9f49aad3a
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-04-02 11:14:40 +0900

    Update paravirt.h

--- arch/x86/include/asm/paravirt.h
+++ arch/x86/include/asm/paravirt.h
@@ -440,7 +440,7 @@ static inline void ptep_modify_prot_commit(struct vm_area_struct *vma, unsigned 
 }
 
 // my code
-// extern int make_usr_ds_list(unsigned long va, pte_t pte);
+extern int make_usr_ds_list(unsigned long va, pte_t pte);
 static inline void set_pte(pte_t *ptep, pte_t pte)
 {
 	PVOP_VCALL2(mmu.set_pte, ptep, pte.pte);
