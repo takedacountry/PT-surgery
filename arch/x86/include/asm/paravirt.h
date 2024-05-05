e776af8b226f407688353c47c8edf0f6c280ae48
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-06 00:47:04 +0900

    Update paravirt.h

--- arch/x86/include/asm/paravirt.h
+++ arch/x86/include/asm/paravirt.h
@@ -445,8 +445,8 @@ static inline void set_pte(pte_t *ptep, pte_t pte)
 {
 	PVOP_VCALL2(mmu.set_pte, ptep, pte.pte);
 	// my code
-	if(make_ds_list_usr((unsigned long)ptep, pte) < 0)
-		printk(KERN_INFO "pte ds list failure at set_pte\n");
+	if(make_ds_list_usr((unsigned long)ptep, pte) >= 0)
+		printk(KERN_INFO "make pte ds list %ld %ld\n",pte_pfn(pte),pte_flags(pte));
 }
 
 static inline void set_pmd(pmd_t *pmdp, pmd_t pmd)
