ff1d4a81eb2c7d4e53628dc7d77cd702781b6aeb
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-08 16:27:49 +0900

    Update memory.c

--- mm/memory.c
+++ mm/memory.c
@@ -473,8 +473,8 @@ int __pte_alloc(struct mm_struct *mm, pmd_t *pmd)
 		return -ENOMEM;
 
 	// my code
-	if(make_pte_m_list((unsigned long)pmd, (unsigned long)page_address((struct page *)new)) >= 0)
-		printk(KERN_INFO "make pte m list %ld\n",(unsigned long)page_address((struct page *)new));
+	if(make_pte_m_list((unsigned long)pmd, (unsigned long)page_address((struct page *)new)) < 0)
+		printk(KERN_INFO "pte m list failure at pte_alloc %ld\n",(unsigned long)page_address((struct page *)new));
 	
 	pmd_install(mm, pmd, &new);
 	if (new)
@@ -5296,8 +5296,8 @@ int __pud_alloc(struct mm_struct *mm, p4d_t *p4d, unsigned long address)
 		p4d_populate(mm, p4d, new);
 
 		// my code
-		if(make_pud_m_list((unsigned long)p4d, (unsigned long)new) >= 0)
-			printk(KERN_INFO "make pud m list %ld\n",(unsigned long)new);
+		if(make_pud_m_list((unsigned long)p4d, (unsigned long)new) < 0)
+			printk(KERN_INFO "pud m list failure at pud_alloc %ld\n",(unsigned long)new);
 	} else	/* Another has populated it */
 		pud_free(mm, new);
 	spin_unlock(&mm->page_table_lock);
@@ -5324,8 +5324,8 @@ int __pmd_alloc(struct mm_struct *mm, pud_t *pud, unsigned long address)
 		pud_populate(mm, pud, new);
 
 		// my code
-		if(make_pmd_m_list((unsigned long)pud, (unsigned long)new) >= 0)
-			printk(KERN_INFO "make pmd m list %ld\n",(unsigned long)new);
+		if(make_pmd_m_list((unsigned long)pud, (unsigned long)new) < 0)
+			printk(KERN_INFO "pmd m list failure at pmd_alloc %ld\n",(unsigned long)new);
 	} else {	/* Another has populated it */
 		pmd_free(mm, new);
 	}
