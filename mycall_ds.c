6b144e174b104ae0befc3a4ca47a937ddd74afcf
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-19 12:47:32 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -24,6 +24,8 @@
 #define SYS_mycall_ds_free 471
 #define SYS_mycall_ds_register_pid 472
 #define SYS_mycall_make_ds_usr_from_pgtable 473
+#define SYS_mycall_ds_search2 474
+#define SYS_mycall_m_search2 475
 
 #define INDEX 1024*1024*1024 //1GB
 #define PGD 512
@@ -33,8 +35,8 @@
 
 int main(void)
 {
-    printf("%ld\n", syscall(SYS_mycall_ds_delete));
-    printf("%ld\n", syscall(SYS_mycall_m_delete));
+    // printf("%ld\n", syscall(SYS_mycall_ds_delete));
+    // printf("%ld\n", syscall(SYS_mycall_m_delete));
     printf("%ld %d\n", syscall(SYS_mycall_ds_register_pid, getpid()), getpid());
     printf("%ld\n", syscall(SYS_mycall_make_ds_usr_from_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
@@ -104,10 +106,10 @@ int main(void)
     printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
     printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
     
-    printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
+    // printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
     
-    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
+    // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
 
     // success
@@ -137,6 +139,11 @@ int main(void)
     // }
     
     free(ma);
+
+    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
+    printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
+    printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
+    
     free(mb);
     // free(mc);
     // free(md);
