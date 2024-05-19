9a28867be794af258c87d346e70635a8934e1b43
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-19 18:21:30 +0900

    Update pgalloc.h

--- arch/x86/include/asm/pgalloc.h
+++ arch/x86/include/asm/pgalloc.h
@@ -55,10 +55,14 @@ extern pgtable_t pte_alloc_one(struct mm_struct *);
 
 extern void ___pte_free_tlb(struct mmu_gather *tlb, struct page *pte);
 
+// my code
+extern void delete_ds_m_free_pte(unsigned long va);
+
 static inline void __pte_free_tlb(struct mmu_gather *tlb, struct page *pte,
 				  unsigned long address)
 {
 	___pte_free_tlb(tlb, pte);
+	delete_ds_m_free_pte((unsigned long)page_address(pte));
 }
 
 static inline void pmd_populate_kernel(struct mm_struct *mm,
