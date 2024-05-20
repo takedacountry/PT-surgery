6819788f24e7834d0a6009707802f8c3fd3d2a94
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-20 15:39:31 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -40,9 +40,9 @@ int main(void)
     printf("%ld %d\n", syscall(SYS_mycall_ds_register_pid, getpid()), getpid());
     printf("%ld\n", syscall(SYS_mycall_make_ds_usr_from_pgtable));
     
-    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
-    printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
-    printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
+    // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+    // printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
+    // printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
     
 
     char *ma = (char*)malloc(INDEX);
@@ -99,15 +99,15 @@ int main(void)
     
     
     // printf("%ld\n", syscall(SYS_mycall_ds_init));
-    // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
     // printf("%ld\n", syscall(SYS_mycall_ds_make_user));
     // printf("%ld\n", syscall(SYS_mycall_make_ds_usr_from_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_ds_make_kernel));
-    // printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
-    // printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
+    printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
+    printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
     
     // printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
