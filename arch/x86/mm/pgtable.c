0777a7d5a829bb3aea41cd3b95bc78b541e46147
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-22 17:25:42 +0900

    Update pgtable.c

--- arch/x86/mm/pgtable.c
+++ arch/x86/mm/pgtable.c
@@ -436,9 +436,9 @@ pgd_t *pgd_alloc(struct mm_struct *mm)
 		goto out;
 
 	// my code
-	if(make_pgd_m_list((unsigned long)pgd) < 0){
-		printk(KERN_INFO "pgd m list failure at pgd_alloc %ld\n",(unsigned long)pgd);
-	}
+	// if(make_pgd_m_list((unsigned long)pgd) < 0){
+	// 	printk(KERN_INFO "pgd m list failure at pgd_alloc %ld\n",(unsigned long)pgd);
+	// }
 
 	mm->pgd = pgd;
 
