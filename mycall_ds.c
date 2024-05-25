b9301324889f2182d18a76382ac1df317b469eeb
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-25 23:22:26 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -47,8 +47,8 @@ int main(void)
 
     char *ma = (char*)malloc(INDEX);
     char *mb = (char*)malloc(INDEX);
-    // char *mc = (char*)malloc(INDEX);
-    // char *md = (char*)malloc(INDEX);
+    char *mc = (char*)malloc(INDEX);
+    char *md = (char*)malloc(INDEX);
     // char *me = (char*)malloc(INDEX);
     // char *mf = (char*)malloc(INDEX);
     // char *mg = (char*)malloc(INDEX);
@@ -64,8 +64,8 @@ int main(void)
     
     memset(ma, 0, INDEX);
     memset(mb, 0, INDEX);
-    // memset(mc, 0, INDEX);
-    // memset(md, 0, INDEX);
+    memset(mc, 0, INDEX);
+    memset(md, 0, INDEX);
     // memset(me, 0, INDEX);
     // memset(mf, 0, INDEX);
     // memset(mg, 0, INDEX);
@@ -82,8 +82,8 @@ int main(void)
 
     printf("va: %p\n", ma); // print user va 
     printf("va: %p\n", mb); // print user va 
-    // printf("va: %p\n", mc); // print user va 
-    // printf("va: %p\n", md); // print user va 
+    printf("va: %p\n", mc); // print user va 
+    printf("va: %p\n", md); // print user va 
     // printf("va: %p\n", me); // print user va
     // printf("va: %p\n", mf); // print user va
     // printf("va: %p\n", mg); // print user va
@@ -157,8 +157,8 @@ int main(void)
     // printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
     // printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
     
-    // free(mc);
-    // free(md);
+    free(mc);
+    free(md);
     // free(me);
     // free(mf);
     // free(mg);
