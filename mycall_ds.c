470aa5eea6599ba465d4b8a797d186415b8da771
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-25 19:22:05 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -99,7 +99,7 @@ int main(void)
     
     
     // printf("%ld\n", syscall(SYS_mycall_ds_init));
-    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+    // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable));
     // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
@@ -109,8 +109,8 @@ int main(void)
     printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
     printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
     
-    printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
-    // printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
+    // printf("%ld\n", syscall(SYS_mycall_recover_all_pgtable));
+    printf("%ld\n", syscall(SYS_mycall_recover_pgtable, 0x0));
     
     // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
     // printf("%ld\n", syscall(SYS_mycall_print_kernel_pgtable2));
