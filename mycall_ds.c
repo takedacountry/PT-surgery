99e23ca13c450adf1302b4261822196952b28a3c
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-05-26 00:03:54 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -53,14 +53,14 @@ int main(void)
     char *mf = (char*)malloc(INDEX);
     char *mg = (char*)malloc(INDEX);
     char *mh = (char*)malloc(INDEX);
-    // char *mi = (char*)malloc(INDEX);
-    // char *mj = (char*)malloc(INDEX);
-    // char *mk = (char*)malloc(INDEX);
-    // char *ml = (char*)malloc(INDEX);
-    // char *mm = (char*)malloc(INDEX);
-    // char *mn = (char*)malloc(INDEX);
-    // char *mo = (char*)malloc(INDEX);
-    // char *mp = (char*)malloc(INDEX);
+    char *mi = (char*)malloc(INDEX);
+    char *mj = (char*)malloc(INDEX);
+    char *mk = (char*)malloc(INDEX);
+    char *ml = (char*)malloc(INDEX);
+    char *mm = (char*)malloc(INDEX);
+    char *mn = (char*)malloc(INDEX);
+    char *mo = (char*)malloc(INDEX);
+    char *mp = (char*)malloc(INDEX);
     
     memset(ma, 0, INDEX);
     memset(mb, 0, INDEX);
@@ -70,14 +70,14 @@ int main(void)
     memset(mf, 0, INDEX);
     memset(mg, 0, INDEX);
     memset(mh, 0, INDEX);
-    // memset(mi, 0, INDEX);
-    // memset(mj, 0, INDEX);
-    // memset(mk, 0, INDEX);
-    // memset(ml, 0, INDEX);
-    // memset(mm, 0, INDEX);
-    // memset(mn, 0, INDEX);
-    // memset(mo, 0, INDEX);
-    // memset(mp, 0, INDEX);
+    memset(mi, 0, INDEX);
+    memset(mj, 0, INDEX);
+    memset(mk, 0, INDEX);
+    memset(ml, 0, INDEX);
+    memset(mm, 0, INDEX);
+    memset(mn, 0, INDEX);
+    memset(mo, 0, INDEX);
+    memset(mp, 0, INDEX);
     
 
     printf("va: %p\n", ma); // print user va 
@@ -88,14 +88,14 @@ int main(void)
     printf("va: %p\n", mf); // print user va
     printf("va: %p\n", mg); // print user va
     printf("va: %p\n", mh); // print user va
-    // printf("va: %p\n", mi); // print user va
-    // printf("va: %p\n", mj); // print user va
-    // printf("va: %p\n", mk); // print user va
-    // printf("va: %p\n", ml); // print user va
-    // printf("va: %p\n", mm); // print user va
-    // printf("va: %p\n", mn); // print user va
-    // printf("va: %p\n", mo); // print user va
-    // printf("va: %p\n", mp); // print user va
+    printf("va: %p\n", mi); // print user va
+    printf("va: %p\n", mj); // print user va
+    printf("va: %p\n", mk); // print user va
+    printf("va: %p\n", ml); // print user va
+    printf("va: %p\n", mm); // print user va
+    printf("va: %p\n", mn); // print user va
+    printf("va: %p\n", mo); // print user va
+    printf("va: %p\n", mp); // print user va
     
     
     // printf("%ld\n", syscall(SYS_mycall_ds_init));
@@ -163,14 +163,14 @@ int main(void)
     free(mf);
     free(mg);
     free(mh);
-    // free(mi);
-    // free(mj);
-    // free(mk);
-    // free(ml);
-    // free(mm);
-    // free(mn);
-    // free(mo);
-    // free(mp);
+    free(mi);
+    free(mj);
+    free(mk);
+    free(ml);
+    free(mm);
+    free(mn);
+    free(mo);
+    free(mp);
     
 
     printf("%ld\n", syscall(SYS_mycall_ds_delete));
