fafee0b89dcfdf5c6a936ec4418178dd5661ee32
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-07-01 19:15:40 +0900

    Create rund.c

---
+++ rund.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	srand(time(NULL));
+	for(i=0;i<10;i++){
+		printf("%d\n",rand()%100);
+	}
+	return 0;
+}
