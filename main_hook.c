9859fcad73d3cb433976519d829fc4f979fa200e
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-07-02 10:56:37 +0900

    Update main_hook.c

--- main_hook.c
+++ main_hook.c
@@ -30,13 +30,19 @@ int main_hook(int argc, char **argv, char **envp)
 {
         printf("register pid %ld %d\n", syscall(SYS_mycall_ds_register_pid, getpid()), getpid());
         printf("make ds usr  %ld\n", syscall(SYS_mycall_make_ds_usr_from_pgtable));
+        printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+        printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
+        printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
         printf("-----Before main-----\n");
         chroot("./");
         chdir("/");
         int ret = main_orig(argc, argv, envp);
         printf("-----after main-----\n");
-        // printf("ds delete %ld\n", syscall(SYS_mycall_ds_delete));
-        // printf("m  delete %ld\n", syscall(SYS_mycall_m_delete));
+        // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
+        // printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
+        // printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
+        printf("ds delete %ld\n", syscall(SYS_mycall_ds_delete));
+        printf("m  delete %ld\n", syscall(SYS_mycall_m_delete));
         printf("main() returned %d\n", ret);
         return ret;
 }
