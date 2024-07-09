bd5e1faa00923e1606d919b6aca9574819c37428
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-07-10 01:15:23 +0900

    Update main_hook.c

--- main_hook.c
+++ main_hook.c
@@ -37,13 +37,13 @@ int main_hook(int argc, char **argv, char **envp)
         chroot("./");
         chdir("/");
         int ret = main_orig(argc, argv, envp);
-        printf("-----after main-----\n");
-        printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
-        printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
-        printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
-        printf("ds delete %ld\n", syscall(SYS_mycall_ds_delete));
-        printf("m  delete %ld\n", syscall(SYS_mycall_m_delete));
-        printf("main() returned %d\n", ret);
+        // printf("-----after main-----\n");
+        // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
+        // printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
+        // printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
+        // printf("ds delete %ld\n", syscall(SYS_mycall_ds_delete));
+        // printf("m  delete %ld\n", syscall(SYS_mycall_m_delete));
+        // printf("main() returned %d\n", ret);
         return ret;
 }
 
