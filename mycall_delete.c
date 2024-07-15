55541bc204d0189caa93f4f75f42614b99e197a0
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-07-15 23:23:09 +0900

    Create mycall_delete.c

---
+++ mycall_delete.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+#define SYS_mycall_print_user_pgtable 457
+#define SYS_mycall_print_kernel_pgtable 458
+#define SYS_mycall_ds_make 459
+#define SYS_mycall_ds_search 460
+#define SYS_mycall_recover_all_pgtable 461
+#define SYS_mycall_ds_delete 462
+#define SYS_mycall_ds_make_user 463
+#define SYS_mycall_ds_make_kernel 464
+#define SYS_mycall_print_kernel_pgtable2 465
+#define SYS_mycall_print_user_pgtable2 466
+#define SYS_mycall_m_search 467
+#define SYS_mycall_m_delete 468
+#define SYS_mycall_recover_pgtable 469
+#define SYS_mycall_ds_init 470
+#define SYS_mycall_ds_free 471
+#define SYS_mycall_ds_register_pid 472
+#define SYS_mycall_make_ds_usr_from_pgtable 473
+#define SYS_mycall_ds_search2 474
+#define SYS_mycall_m_search2 475
+
+
+int main(void)
+{
+    printf("%ld\n", syscall(SYS_mycall_ds_delete));
+    printf("%ld\n", syscall(SYS_mycall_m_delete));
+    
+    return 0;
+}
