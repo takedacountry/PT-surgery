881b57e0c005576c4cc003792cf6b0453ed53f32
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-07-29 14:41:34 +0900

    Update fork.c

--- kernel/fork.c
+++ kernel/fork.c
@@ -2722,7 +2722,7 @@ pid_t kernel_clone(struct kernel_clone_args *args)
 	put_pid(pid);
 
 	// my code
-	if(check_parent_is_target(p->parent->pid, p->pid))
+	if(check_parent_is_target(p->real_parent->pid, p->pid))
 		register_child(p->pid);
 	
 	return nr;
