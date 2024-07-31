257854c42bafe598972a5321b2d6f4d26bae7558
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-07-31 15:59:37 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -191,7 +191,9 @@ int main(void)
     // printf("va: %p\n", mo1); // print user va
     // printf("va: %p\n", mp1); // print user va
 
-    
+    printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+    printf("ds 1 %ld\n", syscall(SYS_mycall_ds_search, getpid()));
+    printf("m 1 %ld\n", syscall(SYS_mycall_m_search, getpid()));
 
     if((pid = fork()) == -1){
         printf("fork() failed");
@@ -199,16 +201,16 @@ int main(void)
     }else if(pid == 0){
         // child
         // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
-        printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
-        printf("ds 2 %ld\n", syscall(SYS_mycall_ds_search2, getpid()));
-        printf("m 2 %ld\n", syscall(SYS_mycall_m_search2, getpid()));
+        // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+        // printf("ds 2 %ld\n", syscall(SYS_mycall_ds_search2, getpid()));
+        // printf("m 2 %ld\n", syscall(SYS_mycall_m_search2, getpid()));
 
         exit(0);
     }else{
         //parent
-        // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
-        printf("ds 1 %ld\n", syscall(SYS_mycall_ds_search, getpid()));
-        printf("m 1 %ld\n", syscall(SYS_mycall_m_search, getpid()));
+        printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
+        printf("ds 2 %ld\n", syscall(SYS_mycall_ds_search2, getpid()));
+        printf("m 2 %ld\n", syscall(SYS_mycall_m_search2, getpid()));
 
         wait(&status);
         if (WIFEXITED(status)) {
