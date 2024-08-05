79e4d162952e854a719752796807e48b58300d5c
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-08-05 16:53:38 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -203,8 +203,8 @@ int main(void)
         // printf("%ld %d\n", syscall(SYS_mycall_ds_register_pid, getpid()), getpid());
         // printf("%ld\n", syscall(SYS_mycall_make_ds_usr_from_pgtable));
         // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable2));
-        // printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
-        // printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
+        printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
+        printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
         
         printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
         printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
