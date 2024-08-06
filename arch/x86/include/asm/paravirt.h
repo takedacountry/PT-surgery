eb3e048eee220c3320979ec251eb3785e237b9a4
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-08-07 00:10:44 +0900

    Update paravirt.h

--- arch/x86/include/asm/paravirt.h
+++ arch/x86/include/asm/paravirt.h
@@ -456,9 +456,13 @@ static inline void set_pte_recover(pte_t *ptep, pte_t pte)
 	PVOP_VCALL2(mmu.set_pte, ptep, pte.pte);
 }
 
+// my code
+extern int make_pte_m_list_from_set_pmd((unsigned long)pmd_va, pmd_t pmd);
 static inline void set_pmd(pmd_t *pmdp, pmd_t pmd)
 {
 	PVOP_VCALL2(mmu.set_pmd, pmdp, native_pmd_val(pmd));
+	// my code
+	make_pte_m_list_from_set_pmd((unsigned long)pmdp, pmd);
 }
 
 static inline pmd_t __pmd(pmdval_t val)
