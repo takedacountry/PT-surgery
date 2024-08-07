854578815c676df147925e251fd2182c7be4b406
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-08-07 15:52:41 +0900

    Update paravirt.h

--- arch/x86/include/asm/paravirt.h
+++ arch/x86/include/asm/paravirt.h
@@ -485,9 +485,13 @@ static inline pmdval_t pmd_val(pmd_t pmd)
 				"mov %%rdi, %%rax", ALT_NOT(X86_FEATURE_XENPV));
 }
 
+// my code
+extern int make_pmd_m_list(unsigned long pud_va, pud_t pud);
 static inline void set_pud(pud_t *pudp, pud_t pud)
 {
 	PVOP_VCALL2(mmu.set_pud, pudp, native_pud_val(pud));
+	// my code
+	make_pmd_m_list((unsigned long)pudp, pud);
 }
 
 static inline pud_t __pud(pudval_t val)
@@ -511,11 +515,15 @@ static inline void pud_clear(pud_t *pudp)
 	set_pud(pudp, native_make_pud(0));
 }
 
+// my code
+extern int make_pud_m_list(unsigned long p4d_va, p4d_t p4d);
 static inline void set_p4d(p4d_t *p4dp, p4d_t p4d)
 {
 	p4dval_t val = native_p4d_val(p4d);
 
 	PVOP_VCALL2(mmu.set_p4d, p4dp, val);
+	// my code
+	make_pud_m_list((unsigned long)p4dp, p4d);
 }
 
 #if CONFIG_PGTABLE_LEVELS >= 5
