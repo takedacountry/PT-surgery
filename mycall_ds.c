9fec70ecf014c1c65323d41975506fcce9029933
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-08-08 23:20:54 +0900

    Update mycall_ds.c

--- mycall_ds.c
+++ mycall_ds.c
@@ -157,7 +157,7 @@ int main(void)
         return -1;
     }else if(pid == 0){
         // child
-        printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
+        // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
         printf("%ld\n", syscall(SYS_mycall_ds_search, getpid()));
         printf("%ld\n", syscall(SYS_mycall_m_search, getpid()));
     
@@ -165,8 +165,8 @@ int main(void)
         // printf("%ld\n", syscall(SYS_mycall_ds_search2, getpid()));
         // printf("%ld\n", syscall(SYS_mycall_m_search2, getpid()));
         // exit(0);
-        printf("%ld\n", syscall(SYS_mycall_ds_delete));
-        printf("%ld\n", syscall(SYS_mycall_m_delete));
+        // printf("%ld\n", syscall(SYS_mycall_ds_delete));
+        // printf("%ld\n", syscall(SYS_mycall_m_delete));
     }else{
         //parent
         // printf("%ld\n", syscall(SYS_mycall_print_user_pgtable));
@@ -239,8 +239,8 @@ int main(void)
         // free(mo1);
         // free(mp1);
     
-        // printf("%ld\n", syscall(SYS_mycall_ds_delete));
-        // printf("%ld\n", syscall(SYS_mycall_m_delete));
+        printf("%ld\n", syscall(SYS_mycall_ds_delete));
+        printf("%ld\n", syscall(SYS_mycall_m_delete));
 
     }
 
