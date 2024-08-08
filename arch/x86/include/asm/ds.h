e646ddda320905b556e396627a8d74d0342d2ae2
takeda kazuki <118811301+takedacountry@users.noreply.github.com>
2024-08-08 15:02:04 +0900

    Update ds.h

--- arch/x86/include/asm/ds.h
+++ arch/x86/include/asm/ds.h
@@ -1,39 +1,39 @@
 #include <linux/types.h>
 
-struct ds_list{
-	unsigned long base;
-	unsigned long limit;
-	long offset;
-	unsigned long flag;
-	struct list_head list;
-};
-
-struct ds_head_list{
-	pid_t pid;
-	struct list_head head;
-	struct list_head list;
-};
-
-// struct ds_list_head{
-// 	struct list_head usr_ds_list;
-// 	struct list_head ker_ds_list;
+// struct ds_list{
+// 	unsigned long base;
+// 	unsigned long limit;
+// 	long offset;
+// 	unsigned long flag;
+// 	struct list_head list;
 // };
 
-struct m_list{
-	unsigned long va;
-	unsigned long num;
-	struct list_head list;
-};
+// struct ds_head_list{
+// 	pid_t pid;
+// 	struct list_head head;
+// 	struct list_head list;
+// };
 
-struct m_head_list{
-	pid_t pid;
-	struct list_head head;
-	struct list_head list;
-};
+// // struct ds_list_head{
+// // 	struct list_head usr_ds_list;
+// // 	struct list_head ker_ds_list;
+// // };
+
+// struct m_list{
+// 	unsigned long va;
+// 	unsigned long num;
+// 	struct list_head list;
+// };
+
+// struct m_head_list{
+// 	pid_t pid;
+// 	struct list_head head;
+// 	struct list_head list;
+// };
 		
-// struct m_list_head{
-// 	struct list_head usr_m_list;
-// 	struct list_head ker_m_list;
+// // struct m_list_head{
+// // 	struct list_head usr_m_list;
+// // 	struct list_head ker_m_list;
 // };
 
 extern struct list_head usr_m_head;
